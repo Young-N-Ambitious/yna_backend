c1f3e99151ef3095e1818a818e6deb9375ec6412
sydnee510 <sbsydnee161416@gmail.com>
2023-03-08 22:35:53 -0800

    Secure API endpoints

--- config/application.rb
+++ config/application.rb
@@ -35,6 +35,9 @@ module YnaBackend
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore, key: '_myapp_session'
+
     config.api_only = true
   end
 end
