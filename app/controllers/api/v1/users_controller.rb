042f1c7302c2c9d351e2ae933e90a5fd592b7581
sydnee510 <sbsydnee161416@gmail.com>
2023-03-08 23:02:31 -0800

    Add user resources to show user in api

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::UsersController < ApplicationController
     
-    before_action :authenticate_user!
+  #  before_action :authenticate_user!
     
     def index
         users = User.all
