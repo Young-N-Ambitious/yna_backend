f1deca6303792df94d0232e33351ef0d46ad6767
Sydnee Sampson <sbsydnee161416@gmail.com>
2023-03-14 20:49:12 +0000

    Update gitpod.yml and config.hosts to launch the app on port 3000

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,6 +61,8 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
+  # Use for initializing the project in gitpod (the url port will open in browser)
+  config.hosts << "3000-youngnambiti-ynabackend-1zn9im5fism.ws-us90.gitpod.io"
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 end
