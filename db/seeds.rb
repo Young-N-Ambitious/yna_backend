043affdfc52f74ea326a2aafc4a53111450cf0b2
Luis Castillo <mr.luiscastillo@gmail.com>
2023-03-15 18:39:11 -0400

    add 10 users Data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,186 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = User.create([
+    {
+    first_name: "John",
+    last_name: "Smith",
+    email: "johnsmith@email.com",
+    pronouns: "he/him/his",
+    linked_in_url: "linkedIn URL",
+    twitter_url: "twitter URL",
+    portfolio_url: "Portfolio URL",
+    industry_title: "Software Engineer",
+    profile_photo: "Picture URL",
+    user_bio: "Personal biography",
+    member: false,
+    founder: false,
+    ceo: false,
+    featured_member: false,
+    password: "123456"
+
+},
+    {
+    first_name: "Will",
+    last_name: "Clark",
+    email: "willclark@email.com",
+    pronouns: "he/him/his",
+    linked_in_url: "linkedIn URL",
+    twitter_url: "twitter URL",
+    portfolio_url: "Portfolio URL",
+    industry_title: "Software Engineer",
+    profile_photo: "Picture URL",
+    user_bio: "Personal biography",
+    member: false,
+    founder: false,
+    ceo: false,
+    featured_member: false,
+    password: "123456"
+
+},
+    {
+    first_name: "Olivia",
+    last_name: "Park",
+    email: "oliviapark@email.com",
+    pronouns: "she/her/hers",
+    linked_in_url: "linkedIn URL",
+    twitter_url: "twitter URL",
+    portfolio_url: "Portfolio URL",
+    industry_title: "UX/UI",
+    profile_photo: "Picture URL",
+    user_bio: "Personal biography",
+    member: false,
+    founder: false,
+    ceo: false,
+    featured_member: false,
+    password: "123456"
+
+},
+    {
+    first_name: "Emma",
+    last_name: "James",
+    email: "emmajames@email.com",
+    pronouns: "she/her/hers",
+    linked_in_url: "linkedIn URL",
+    twitter_url: "twitter URL",
+    portfolio_url: "Portfolio URL",
+    industry_title: "UX/UI",
+    profile_photo: "Picture URL",
+    user_bio: "Personal biography",
+    member: false,
+    founder: false,
+    ceo: false,
+    featured_member: false,
+    password: "123456"
+
+},
+    {
+    first_name: "Charlotte",
+    last_name: "Davis",
+    email: "charlottedavis@email.com",
+    pronouns: "she/her/hers",
+    linked_in_url: "linkedIn URL",
+    twitter_url: "twitter URL",
+    portfolio_url: "Portfolio URL",
+    industry_title: "UX/UI",
+    profile_photo: "Picture URL",
+    user_bio: "Personal biography",
+    member: false,
+    founder: false,
+    ceo: false,
+    featured_member: false,
+    password: "123456"
+
+},
+    {
+    first_name: "Amelia",
+    last_name: "Garcia",
+    email: "ameliagarcia@email.com",
+    pronouns: "she/her/hers",
+    linked_in_url: "linkedIn URL",
+    twitter_url: "twitter URL",
+    portfolio_url: "Portfolio URL",
+    industry_title: "UX/UI",
+    profile_photo: "Picture URL",
+    user_bio: "Personal biography",
+    member: false,
+    founder: false,
+    ceo: false,
+    featured_member: false,
+    password: "123456"
+
+},
+    {
+    first_name: "Elijah",
+    last_name: "Brown",
+    email: "elijahbrown@email.com",
+    pronouns: "he/him/his",
+    linked_in_url: "linkedIn URL",
+    twitter_url: "twitter URL",
+    portfolio_url: "Portfolio URL",
+    industry_title: "DevOps Engineer",
+    profile_photo: "Picture URL",
+    user_bio: "Personal biography",
+    member: false,
+    founder: false,
+    ceo: false,
+    featured_member: false,
+    password: "123456"
+
+},
+    {
+    first_name: "Lucas",
+    last_name: "Marinez",
+    email: "lucasmartinez@email.com",
+    pronouns: "he/him/his",
+    linked_in_url: "linkedIn URL",
+    twitter_url: "twitter URL",
+    portfolio_url: "Portfolio URL",
+    industry_title: "ML Engineer",
+    profile_photo: "Picture URL",
+    user_bio: "Personal biography",
+    member: false,
+    founder: false,
+    ceo: false,
+    featured_member: false,
+    password: "123456"
+
+},
+    {
+    first_name: "Noah",
+    last_name: "Taylor",
+    email: "noahtaylor@email.com",
+    pronouns: "he/him/his",
+    linked_in_url: "linkedIn URL",
+    twitter_url: "twitter URL",
+    portfolio_url: "Portfolio URL",
+    industry_title: "UX/UI",
+    profile_photo: "Picture URL",
+    user_bio: "Personal biography",
+    member: false,
+    founder: false,
+    ceo: false,
+    featured_member: false,
+    password: "123456"
+
+},
+    {
+    first_name: "James",
+    last_name: "Moore",
+    email: "jamesmoore@email.com",
+    pronouns: "he/him/his",
+    linked_in_url: "linkedIn URL",
+    twitter_url: "twitter URL",
+    portfolio_url: "Portfolio URL",
+    industry_title: "UX/UI",
+    profile_photo: "Picture URL",
+    user_bio: "Personal biography",
+    member: false,
+    founder: false,
+    ceo: false,
+    featured_member: false,
+    password: "123456"
+
+}
+])
