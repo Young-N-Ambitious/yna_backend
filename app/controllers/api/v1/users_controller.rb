3f85060b2be99c0efee1ce1608a6d16936a34e9b
Sydnee Sampson <sbsydnee161416@gmail.com>
2023-03-15 04:04:05 +0000

    Update user controller and user model with validations

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::UsersController < ApplicationController
-    
-  #  before_action :authenticate_user!
-    
+ # before_action :authenticate_user!, only: [:update, :destroy] 
+   
     def index
         users = User.all
         render json: users
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,47 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  rolify role_cname: 'Role'
+  
+  #Associations
+  has_many :user_roles, dependent: :destroy
+  has_many :roles, through: :user_roles
+
+  #Validations
+  validates :first_name, :last_name, :pronouns, :linked_in_url, :twitter_url, :portfolio_url, :industry_title, :profile_photo, :user_bio, presence: true
+  validates :email, uniqueness: true
+  validates :password, length: { minimum: 8 }
+  has_one_attached :profile_photo
+
+  before_validation :assign_default_role
+
+  #Helper Methods
+  def assign_default_role
+    self.roles << Role.find_by_name('member') if self.roles.empty?
+  end
+
+  def role?(role)
+    roles.any? { |r| r.name.underscore.to_sym == role }
+  end
+
+  def member?
+    role?(:member)
+  end
+
+  def ceo?
+    role?(:ceo)
+  end
+
+  def founder?
+    role?(:founder)
+  end
+
+  def featured_member?
+    role?(:featured_member)
+  end
+
+  def complete_profile?
+    first_name.present? && last_name.present? && pronouns.present? && linked_in_url.present? && twitter_url.present? && portfolio_url.present? && industry_title.present? && profile_photo.attached? && user_bio.present?
+  end       
 end
