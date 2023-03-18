1f3810965c72231b50bc319adf175bccbf2e1599
Sydnee Sampson <sbsydnee161416@gmail.com>
2023-03-18 22:26:48 +0000

    Update the user serializer attributes

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :member_id, :first_name, :last_name, :pronouns, :twitter_url, :linked_in_url, :portfolio_url, :industry_title, :profile_photo, :user_bio, :member, :founder, :admin, :ceo, :featured_member
-  
-  has_many :email
+  attributes :id, :email, :first_name, :last_name, :pronouns, :twitter_url, :linked_in_url, :portfolio_url, :industry_title, :profile_photo, :user_bio, :member, :founder, :ceo, :featured_member
 end
